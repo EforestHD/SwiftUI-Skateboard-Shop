7a4a03674da6dabea249246d0b8a1a31d4b978d1
Ewald <57798772+EforestHD@users.noreply.github.com>
2020-11-25 16:03:33 +0100

    Update and rename Accessiors.swift to Demo.swift

---
+++ build/Demo.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct List: View {
+    var body: some View {
+        VStack {
+        Text("Hello my fella GitHub User :)")
+        Text("If you want my full code, please feel free to contact me here on GitHub")
+        }
+    }
+}
+
+
