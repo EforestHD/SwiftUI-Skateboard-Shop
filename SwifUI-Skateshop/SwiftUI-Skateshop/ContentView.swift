06c9b30febf09afbda657a189b285947583c8c00
Ewald <57798772+EforestHD@users.noreply.github.com>
2022-04-08 13:13:12 +0200

    init

---
+++ SwifUI-Skateshop/SwiftUI-Skateshop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Skateshop
+//
+//  Created by Ewald Kerner on 08.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Please feel free to contact me on Twitter @EwaldKerner")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwifUI-Skateshop/SwiftUI-Skateshop/SwiftUI_SkateshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SkateshopApp.swift
+//  SwiftUI-Skateshop
+//
+//  Created by Ewald Kerner on 08.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SkateshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
